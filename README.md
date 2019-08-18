0280dec9f949293c79e9967cd3f58dc7d217b1c4
Jason Brewer <jason@urawizard.com>
2019-08-18 21:20:40 +0100

    Clone all repos - have a good time

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # MonMon-ops
+
+At present:
+
+- clones all repos to a local *NIX machine with a handy `Code` workspace (`./begin.sh`)
+
+At future:
+
+- Run the blob of services
+- deploy stuff
+- run tests (local Jenkins server/Travis config?)
---
+++ begin.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+git clone git@github.com:InBrewJ/MonMon-mobile.git ../MonMon-mobile
+git clone git@github.com:InBrewJ/MonMon-middleman.git ../MonMon-middleman
+git clone git@github.com:InBrewJ/MonMon-vault.git ../MonMon-vault
